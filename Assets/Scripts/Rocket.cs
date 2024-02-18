4c43f43950730a270df16dab078960c40582d4a5
BigCoderDominykas <146240089+BigCoderDominykas@users.noreply.github.com>
2024-02-18 16:47:40 +0200

    New weapon

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,7 +4,10 @@ public class Rocket : MonoBehaviour
 {
     public float speed = 20;
     public int damage = 10;
+    public int bounceCount = 0;
     public GameObject explosionParticles;
+    public GameObject bounceParticles;
+    public GameObject bulletHole;
 
     private void Start()
     {
@@ -24,11 +27,21 @@ public class Rocket : MonoBehaviour
             health.Damage(damage);
         }
 
-        Destroy(gameObject);
-    }
+        var obj = Instantiate(bulletHole, transform.position, Quaternion.identity);
+        obj.transform.forward = collision.contacts[0].normal;
 
-    private void OnDestroy()
-    {
-        Instantiate(explosionParticles, transform.position, Quaternion.identity);
+        if (bounceCount > 0)
+        {
+            transform.forward = collision.contacts[0].normal;
+            bounceCount--;
+            // Wall hit effect
+            obj = Instantiate(bounceParticles, transform.position, Quaternion.identity);
+            obj.transform.forward = collision.contacts[0].normal;
+        }
+        else
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticles, transform.position, Quaternion.identity);
+        }
     }
 }
