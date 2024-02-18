d51993f7a2ef3a1d051766b085b1be539a86eb85
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-18 16:15:09 +0200

    Rocket explode, bounce

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,20 +2,41 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-	public float speed = 20;
+	public float speed = 20f;
+	public int damage = 10;
+	public GameObject explosionPrefab;
+	public GameObject hitPrefab;
+	public int bounceCount;
 
-	void Start()
+	private void Start()
 	{
-		Destroy(gameObject,3f);
+		Destroy(gameObject, 3f);
 	}
 
-	void Update()
+	private void Update()
 	{
 		transform.position += transform.forward * speed * Time.deltaTime;
 	}
 
-	void OnCollisionEnter(Collision other)
+	private void OnCollisionEnter(Collision other)
 	{
-		Destroy(gameObject);
+		var health = other.gameObject.GetComponent<Health>();
+		if(health != null)
+		{
+			health.Damage(damage);
+		}
+		// Wall hit effect
+		var obj = Instantiate( hitPrefab, transform.position, Quaternion.identity);
+		obj.transform.forward = other.contacts[0].normal;
+
+		if (bounceCount > 0)
+		{
+			transform.forward =  other.contacts[0].normal;
+			bounceCount--;
+		}else
+		{
+			Destroy(gameObject);
+			Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+		}
 	}
 }
