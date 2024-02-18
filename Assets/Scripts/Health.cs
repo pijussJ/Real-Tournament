1583735738d0077a9c6a3e798d5c4072a0cbb833
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-18 14:46:07 +0200

    Health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+	[SerializeField]int health;
+	public int maxHealth = 100;
+	
+	void Start()
+	{
+		if(health == 0) health = maxHealth;
+	}
+
+	public void Damage(int damage)
+	{
+		health -= damage;
+		if(health <= 0)
+		{
+			Die();
+		}
+	}
+
+	public void Die()
+	{
+		Destroy(gameObject);
+	}
+}
