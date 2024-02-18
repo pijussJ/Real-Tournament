f85bdd903637f354f4dabb02bcfed3338ea7ae17
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-18 15:08:08 +0200

    AsyncAwait, Reloading

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,11 +4,44 @@ public class Weapon : MonoBehaviour
 {
     public GameObject bulletPrefab;
 
+    public int ammo;
+    public int maxAmmo = 10;
+    public bool isReloading;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Instantiate(bulletPrefab,transform.position,transform.rotation);
+	        Shoot();
         }
+        if( Input.GetKeyDown(KeyCode.R) && ammo < maxAmmo)
+        {
+            Reload();
+        }
+    }
+
+    void Shoot()
+    {
+        if (isReloading) return;
+        if (ammo <= 1)
+        {
+            Reload();
+            return;
+        }
+
+        ammo--;
+        Instantiate(bulletPrefab,transform.position,transform.rotation);
+    }
+
+
+    async void Reload()
+    {
+        if(isReloading) return;
+
+        isReloading = true;
+        await new WaitForSeconds(2);
+        isReloading = false;
+        ammo = maxAmmo;
+        print("Reloaded");
     }
 }
