b5f4cafe3dacfb98ca84ab9414be046ca8949a5f
BigCoderDominykas <146240089+BigCoderDominykas@users.noreply.github.com>
2024-02-22 16:31:19 +0200

    Added bullet spread and multibullet support

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,6 +6,8 @@ public class Weapon : MonoBehaviour
     public int maxAmmo;
     public bool isAutoFire;
     public float shootInterval = 0.5f;
+    public int multiBullet = 1;
+    public float spread;
 
     bool isReloading;
     float shootCooldown;
@@ -43,14 +45,23 @@ public class Weapon : MonoBehaviour
     {
         if (isReloading) return;
         if (shootCooldown > 0) return;
-        if (ammo <= 1)
+        //if (ammo <= 1)
+        if (ammo <= multiBullet)
         {
             Reload();
         }
 
-        ammo--;
         shootCooldown = shootInterval;
-        Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+        for (int i = 0; i < multiBullet; i++)
+        {
+            if (ammo > 0)
+            {
+                ammo--;
+                var rot = transform.rotation * Quaternion.Euler(Random.Range(-spread, spread), Random.Range(-spread, spread), 0);
+                Instantiate(bulletPrefab, transform.position, rot);
+            }
+        }
     }
 
     async void Reload()
